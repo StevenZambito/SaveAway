af30142beb84596e4e89a4262f0b9e1a3c859569
StevenZambito <zambito.steven@icloud.com>
2021-05-12 10:20:34 -0400

    Styled page with all the goal boxes

--- ClientApp/src/components/Header.jsx
+++ ClientApp/src/components/Header.jsx
@@ -3,7 +3,10 @@ import styles from '../styles/Header.module.scss'
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>Save Away</h1>
+      <div>
+        <h1>Save Away</h1>
+        <button>Log Out</button>
+      </div>
     </header>
   )
 }
--- ClientApp/src/pages/SaveAway.jsx
+++ ClientApp/src/pages/SaveAway.jsx
@@ -7,6 +7,12 @@ export function SaveAway() {
       <div className={styles.saveAwayPage}>
         <Header />
         <div className={styles.goalBoxContainer}>
+          <div className={styles.addGoalBox}>
+            <div className={styles.emoji}>
+              <p>+</p>
+              <p>New Goal</p>
+            </div>
+          </div>
           <div className={styles.goalBox}>
             <div className={styles.emoji}>
               <p>🚗</p>
@@ -14,11 +20,105 @@ export function SaveAway() {
             </div>
 
             <div className={styles.numberInfo}>
-              <p>Target Amount: $300000</p>
-              <p>Amount Saved: $300</p>
+              <p className={styles.targetAndAmount}>
+                Target Amount: <p>$3000</p>
+              </p>
+              <p className={styles.targetAndAmount}>
+                Amount Saved: <p>$300</p>
+              </p>
             </div>
             <div className={styles.progressBar}>
-              <progress value="30" max="50" />
+              <progress value="300" max="3000" />
+            </div>
+          </div>
+          <div className={styles.goalBox}>
+            <div className={styles.emoji}>
+              <p>🎄</p>
+              <p>Christmas</p>
+            </div>
+
+            <div className={styles.numberInfo}>
+              <p className={styles.targetAndAmount}>
+                Target Amount: <p>$2000</p>
+              </p>
+              <p className={styles.targetAndAmount}>
+                Amount Saved: <p>$450</p>
+              </p>
+            </div>
+            <div className={styles.progressBar}>
+              <progress value="450" max="2000" />
+            </div>
+          </div>
+          <div className={styles.goalBox}>
+            <div className={styles.emoji}>
+              <p>🏖</p>
+              <p>Hawaii Vacation</p>
+            </div>
+
+            <div className={styles.numberInfo}>
+              <p className={styles.targetAndAmount}>
+                Target Amount: <p>$40000</p>
+              </p>
+              <p className={styles.targetAndAmount}>
+                Amount Saved: <p>$20000</p>
+              </p>
+            </div>
+            <div className={styles.progressBar}>
+              <progress value="20000" max="40000" />
+            </div>
+          </div>
+          <div className={styles.goalBox}>
+            <div className={styles.emoji}>
+              <p>🎟</p>
+              <p>Concert Tickets</p>
+            </div>
+
+            <div className={styles.numberInfo}>
+              <p className={styles.targetAndAmount}>
+                Target Amount: <p>$1000</p>
+              </p>
+              <p className={styles.targetAndAmount}>
+                Amount Saved: <p>$700</p>
+              </p>
+            </div>
+            <div className={styles.progressBar}>
+              <progress value="700" max="1000" />
+            </div>
+          </div>
+          <div className={styles.goalBox}>
+            <div className={styles.emoji}>
+              <p>🎉</p>
+              <p>Birthday Party</p>
+            </div>
+
+            <div className={styles.numberInfo}>
+              <p className={styles.targetAndAmount}>
+                Target Amount: <p>$500</p>
+              </p>
+              <p className={styles.targetAndAmount}>
+                Amount Saved: <p>$400</p>
+              </p>
+            </div>
+            <div className={styles.progressBar}>
+              <progress value="400" max="500" />
+            </div>
+          </div>
+          <div className={styles.goalBox}>
+            <div className={styles.emoji}>
+              <p>💻</p>
+              <p>New Laptop</p>
+            </div>
+
+            <div className={styles.numberInfo}>
+              <p className={styles.targetAndAmount}>
+                Target Amount: <p>$2500</p>
+              </p>
+              <p className={styles.targetAndAmount}>
+                Amount Saved: <p>$1000</p>
+              </p>
+            </div>
+            <div className={styles.progressBar}>
+              <progress value="1000" max="2500" />
             </div>
           </div>
         </div>
